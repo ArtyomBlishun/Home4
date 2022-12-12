a9589573a53a717fae0bee12c5f549707cbca3a1
Artyom Blishun <ablishun@mail.ru>
2022-12-12 21:42:33 +0300

    first  commit

---
+++ Задача29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] array = new int[8];
+PrintArray(array);
+Console.WriteLine();
+PrintArray(FillAray(array));
+Console.WriteLine();
+PrintArray(array);
+
+int[] FillAray(int[] arr)
+{
+    int[] result = new int[arr.Length];
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(0, 1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
